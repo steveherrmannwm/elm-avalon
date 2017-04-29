d6d34c2660def0a7ac9c9596b6b4df240a1ca016
Steve Herrmann <steven.herrmannwm@gmail.com>
2017-04-28 22:46:20 -0600

    Prepped for a push to heroku

---
+++ server.js
@@ -0,0 +1,245 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+var rooms = {};
+// TODO: Convert to using broadcast channels
+function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+function findObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+app.use(function (req, res, next) {
+  req.testing = 'testing';
+  return next();
+});
+
+
+app.ws('/gen_room', function(ws, req) {
+  ws.on('message', function(msg) {
+    var code = "";
+    while(code == "" || Object.keys(rooms).indexOf(code) > 1){
+      code = randomString(5, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    }
+    ws.send(code)
+    rooms[code] = {"users": {}, "roles": false}
+
+    app.ws('/' + code, function(ws, req) {
+      ws.on('message', function(msg){
+          var parsed = JSON.parse(msg)
+          if(findObject(ws, rooms[code]) < 0)
+          {
+            rooms[code]["users"][parsed["name"]] = {"connections": {"chat": ws}};
+          }
+          for(var key in rooms[code]["users"]){
+            rooms[code]["users"][key]["connections"]["chat"].send(parsed["name"] + ": " + parsed["msg"])
+          }
+
+
+        })
+      ws.on('close', function(){
+        // Remove the connection from the list, and broadcast a message to other clients
+        var dc = ""
+        for(var key in rooms[code]["users"]){
+          if (rooms[code]["users"][key]["connections"]["chat"] === ws)
+          {
+            delete rooms[code]["users"][key];
+            dc = key;
+          }
+        }
+
+        for(var key in rooms[code]["users"]){
+          rooms[code]["users"][key]["connections"]["chat"].send(dc + " has disconnected")
+        }
+
+        if(Object.keys(rooms[code]["users"]).length == 0)
+        {
+          delete rooms[code]
+          console.log(code + " ended");
+        }
+      })
+      })
+  });
+});
+
+app.ws('/join_room', function(ws, req){
+  ws.on('message', function(msg) {
+    console.log("JOIN " + msg)
+    if(Object.keys(rooms).indexOf(msg) >= 0)
+    {
+      ws.send('OK')
+    }
+    else {
+      ws.send("Room isn't registered")
+    }
+  })
+})
+
+app.ws('/player_list', function(ws, req) {
+  ws.on("message", function(msg){
+    var parsed = JSON.parse(msg)
+    console.log(Object.keys(rooms[parsed["room"]]["users"]))
+    ws.send(Object.keys(rooms[parsed["room"]]["users"]).join(","))
+  });
+});
+
+
+function generateRoles(players)
+{
+  var roles = {"good": ["Member", "Percival"],
+               "evil": ["Minion", "Morgana", "Oberon", "Mordred"]
+             };
+  var templates = {5: {"Good": 3, "Evil" : 1, "Assassin": 1},
+                   6: {"Good": 4, "Evil": 1, "Assassin": 1},
+                   7: {"Good": 4, "Evil": 2, "Assassin": 1},
+                   8: {"Good": 5, "Evil": 2, "Assassin": 1},
+                   9: {"Good": 6, "Evil": 2, "Assassin": 1},
+                   10: {"Good": 6, "Evil": 3, "Assassin": 1},
+                 };
+   var goodCount = templates[players]["Good"] - 1; // Remove 1 for Merlin, who always exists
+   var evilCount = templates[players]["Evil"];
+   var assassinCount = templates[players]["Assassin"];;
+
+   var acc = [["Merlin", "Good"]]
+   for (var i = 1; i < players; i++)
+   {
+     if(goodCount > 0)
+     {
+       var index = Math.floor(Math.random() * roles["good"].length);
+       var role = roles["good"][index];
+       if(index > 0) // Want to keep member around permanently
+       {
+          roles["good"].splice(index, 1)
+       }
+       acc[i] = [role, "Good"];
+       goodCount--;
+     }
+     else {
+       var index = Math.floor(Math.random() * roles["evil"].length);
+       var role = roles["evil"][index];
+       if(index > 0) // Want to keep Minion around permanently
+       {
+         roles["evil"].splice(index, 1)
+       }
+       var align = "Evil"
+       if (assassinCount > 0)
+       {
+         var flip = Math.floor(Math.random() * (assassinCount + evilCount));
+         if(flip == 0 || evilCount == 0)
+         {
+           align = "Assassin";
+           assassinCount--;
+         }
+         else {
+           evilCount--;
+         }
+       }
+       acc[i] = [role, align];
+     }
+   }
+   return acc;
+}
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+    return a
+}
+
+app.ws('/create_roles', function(ws, req) {
+
+  ws.on("message", function(msg){
+    var parsed = JSON.parse(msg)
+    if (parsed["maxPlayers"] - Object.keys(rooms[parsed['room']]['users']).length == 0)
+    {
+      if(!rooms[parsed['room']]['roles']){
+        var roles = shuffle(generateRoles(parsed['maxPlayers']))
+        console.log("Creating roles")
+        var index = 0;
+        for(var key in rooms[parsed['room']]["users"]){
+          rooms[parsed['room']]['users'][key]['role'] = roles[index];
+          index++;
+        }
+        rooms[parsed['room']]['roles'] = true;
+      }
+      ws.send("OK")
+    }
+    else {
+      ws.send("NEP")
+    }
+
+  });
+});
+
+app.ws('/retrieve_role', function(ws, req) {
+  ws.on("message", function(msg){
+    var parsed = JSON.parse(msg)
+    console.log(Object.keys(rooms[parsed["room"]]["users"]))
+    ws.send(rooms[parsed["room"]]["users"][parsed['user']]['role'].join(","))
+  });
+});
+
+app.ws('/char_info', function(ws, req) {
+  ws.on("message", function(msg){
+    var parsed = JSON.parse(msg)
+    var role = rooms[parsed['room']]['users'][parsed['user']]['role']
+
+    // Special characters
+    var acc = []
+    var sent = false
+    switch(role[0]){
+      case "Merlin":
+        for(var key in rooms[parsed['room']]["users"]){
+          var char_role = rooms[parsed['room']]['users'][key]['role'];
+          if(char_role[1] != "Good" && char_role[0] != "Mordred")
+            acc.push(key)
+        }
+        ws.send("As Merlin, you revealed " + acc.join(", ") + " to be evil");
+        break;
+      case "Percival":
+        for(var key in rooms[parsed['room']]["users"]){
+          var char_role = rooms[parsed['room']]['users'][key]['role'];
+          if(char_role[0] == "Merlin" || char_role[0] == "Morgana")
+            acc.push(key)
+        }
+        ws.send("As Percival, you revealed " + acc.join(", ") + " to be Merlin");
+        break;
+    }
+    // This case is ignored by our specials above, as their both always good
+    // so we can still use acc safely
+    if(role[1] != "Good" && role[0] != "Oberon")
+    {
+      for(var key in rooms[parsed['room']]["users"]){
+        var char_role = rooms[parsed['room']]['users'][key]['role'];
+        if(char_role[1] != 'Good' && char_role['0'] != "Oberon")
+          acc.push(key)
+      }
+      ws.send("As an evil-doer, you know your fellow evildoers are " + acc.join(', '))
+    }
+    else if(role[0] == "Member"){
+      ws.send("As a member, you have no one revealed! Pay close attention to the conversation to try and get info")
+    }
+    else if (role[0] == "Oberon")
+      ws.send("As Oberon, you don't know who your fellow evil-does are! Pay close attention to the conversation to try and get info")
+
+    });
+});
+
+app.listen(8080);
--- src/index.elm
+++ src/index.elm
@@ -4,9 +4,11 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Random exposing (..)
-import Json.Decode as Json
+import Json.Encode exposing (..)
+import Json.Decode exposing (map, decodeValue)
 import Html.Events.Extra exposing (targetValueIntParse)
 import WebSocket
+import List.Extra exposing (..)
 
 
 main =
@@ -19,7 +21,7 @@ main =
 
 wsServer : String
 wsServer =
-  "ws://localhost:8080/"
+  "ws://sample-env-1.94pd2phgp7.us-east-2.elasticbeanstalk.com/:8080/"
 
 roomGen : String
 roomGen = wsServer++"gen_room"
@@ -27,10 +29,22 @@ roomGen = wsServer++"gen_room"
 joinRoom : String
 joinRoom = wsServer++"join_room"
 
--- Type definitions
-type LobbyRole = Host | Client | Waiting
--- Need a better name for this attribute
+playerList : String
+playerList = wsServer++"player_list"
 
+createRoles : String
+createRoles = wsServer++"create_roles"
+
+retrieveRole : String
+retrieveRole = wsServer++"retrieve_role"
+
+charInfo : String
+charInfo = wsServer++"char_info"
+
+setQuestMembers : String
+setQuestMembers = wsServer++"set_quest_members"
+
+-- Type definitions
 
 -- Define possible roles for players
 -- TODO: Come up with original names for these roles
@@ -44,19 +58,33 @@ type Role
   | Mordred
   | Unassigned
 
+type Alignment
+  = Good
+  | Evil
+  | Assassin
+  | Unaligned
+
 type GameState
   = Home
   | Setup
   | TeamBuild
   | Vote
+  | Decide
+  | Wait
   | Final
 
 type alias Player =
   { name : String
   , role : Role
-  , lobbyRole : LobbyRole
+  , alignment: Alignment
   }
 
+type alias Quest =
+  { name: String
+  , playersRequired : Int
+  , players : List String
+  , votes : Int
+  }
 
 -- MODEL
 
@@ -65,15 +93,20 @@ type alias Model =
   { user : Player
   , room : String
   , maxPlayers : Int
+  , currentPlayers : List String -- Only contains usernames to not give away roles
   , chatBox : String
   , chatMessages : List String
   , state : GameState
+  , leaderPosition : Int
+  , revealedInfo : String
+  , quest : Quest
+  , errors : String
   }
 
 
 model : Model
 model =
-  Model (Player "" Unassigned Waiting) "" 0 "" [] Home
+  Model (Player "" Unassigned Unaligned) "" 5 [] "" [] Home -1 "" (Quest "" 2 [] 0) ""
 
 
 -- INIT
@@ -91,19 +124,76 @@ type Msg
     | RoomCode String
     | JoinRoom String
     | SetMaxPlayers Int
+    | PlayerList String
+    | CreateRoles String
+    | RetrieveRole String
     | GenRoomCode
     | GenRoom String
     | SetRoom
     | Input String
     | Send
+    | UpdateQuestTeam String
+    | SubmitQuestTeam
     | NewMessage String
+    | CharInfo String
 
+getListPosition: List String -> Int -> String
+getListPosition list index =
+  (checkList (List.head (List.drop index list)))
+
+checkList : Maybe String -> String
+checkList str =
+  case str of
+    Just string -> string
+    Nothing -> ""
+
+readAlign : String -> Alignment
+readAlign str =
+  case str of
+    "Good" -> Good
+    "Evil" -> Evil
+    "Assassin" -> Assassin
+    _ -> Unaligned
+
+readRole : String -> Role
+readRole str =
+  case str of
+    "Merlin" -> Merlin
+    "Member" -> Member
+    "Minion" -> Minion
+    "Percival" -> Percival
+    "Morgana" -> Morgana
+    "Oberon" -> Oberon
+    "Mordred" -> Mordred
+    _ -> Unassigned
+
+parseRoleResponse: String -> Player -> Player
+parseRoleResponse str player =
+  parseRoleResp (String.split "," str) player
+
+parseRoleResp: List String -> Player -> Player
+parseRoleResp list player =
+  Player player.name (parseRole (List.head list)) (parseAlignment (List.head (List.drop 1 list)))
+
+parseRole: Maybe String -> Role
+parseRole str =
+  case str of
+    Just string -> readRole string
+    Nothing ->
+      Unassigned
+
+parseAlignment: Maybe String -> Alignment
+parseAlignment str =
+  case str of
+    Just string -> readAlign string
+    Nothing ->
+      Unaligned
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Name name ->
-      ({ model | user = Player name Unassigned Waiting }, Cmd.none)
+      ({ model | user = Player name Unassigned Unaligned}, Cmd.none)
 
     RoomCode room ->
       ({ model | room = room},
@@ -111,21 +201,36 @@ update msg model =
 
     JoinRoom response ->
       if response == "OK" then
-        ({ model | state = Setup}, WebSocket.send (wsServer ++ model.room)
-          (model.user.name ++ " has entered the room"))
+        ({ model | state = Setup, errors = ""}, WebSocket.send (wsServer ++ model.room)
+          (Json.Encode.encode 0 (Json.Encode.object [("name", string model.user.name), ("msg", string "has connected")])))
       else
-        (model, Cmd.none)
-        -- TODO: Add error handling
+        ({model | errors = response}, Cmd.none)
 
     SetRoom ->
-      ({model | user = Player model.user.name Unassigned Client}, WebSocket.send joinRoom model.room)
+      ({model | user = Player model.user.name Unassigned Unaligned}, WebSocket.send joinRoom model.room)
 
     GenRoom room ->
-      ({model | room = room, user = Player model.user.name Unassigned Host}, WebSocket.send joinRoom room)
+      ({model | room = room, user = Player model.user.name Unassigned Unaligned}, WebSocket.send joinRoom room)
 
     GenRoomCode ->
       (model, WebSocket.send roomGen "")
-      -- TODO: Figure out if a room code is already taken
+
+    PlayerList list ->
+      ({model | currentPlayers = String.split "," list}, WebSocket.send createRoles
+      (Json.Encode.encode 0 (Json.Encode.object [("room", string model.room), ("maxPlayers", Json.Encode.int model.maxPlayers)])))
+
+    CreateRoles response ->
+      if response /= "NEP" then -- No Enough players
+        (model, WebSocket.send retrieveRole (Json.Encode.encode 0 (Json.Encode.object [("room", string model.room), ("user", string model.user.name)])))
+      else
+        (model, Cmd.none)
+
+    CharInfo response ->
+      ({model | revealedInfo = response, state = TeamBuild, errors = "", leaderPosition = (model.leaderPosition + 1) % List.length (model.currentPlayers)}, Cmd.none)
+
+    RetrieveRole response ->
+      ({model | user = parseRoleResponse response model.user},
+      WebSocket.send charInfo (Json.Encode.encode 0 (Json.Encode.object [("room", string model.room), ("user", string model.user.name)])))
 
     SetMaxPlayers players ->
       ({ model | maxPlayers = players}, Cmd.none)
@@ -133,11 +238,23 @@ update msg model =
     Input newInput ->
       ({model | chatBox = newInput}, Cmd.none)
 
+    UpdateQuestTeam player ->
+      if (List.member player model.quest.players) then
+        ({model | quest = Quest model.quest.name model.quest.playersRequired (List.Extra.remove player model.quest.players) model.quest.votes}, Cmd.none)
+      else
+        ({model | quest = Quest model.quest.name model.quest.playersRequired (player::model.quest.players) model.quest.votes}, Cmd.none)
+
     Send ->
-      (model, WebSocket.send (wsServer ++ model.room) (model.user.name ++ ": " ++ model.chatBox))
+      (model, WebSocket.send (wsServer ++ model.room)
+      (Json.Encode.encode 0 (Json.Encode.object [("name", string model.user.name), ("msg", string model.chatBox)])))
+      -- model.user.name ++ ": " ++ model.chatBox
 
     NewMessage str ->
-      ({model | chatMessages = (str :: model.chatMessages)}, Cmd.none)
+      ({model | chatMessages = (str :: model.chatMessages)}, WebSocket.send playerList
+      (Json.Encode.encode 0 (Json.Encode.object [("name", string model.user.name), ("room", string model.room)])))
+
+    SubmitQuestTeam ->
+      (model, WebSocket.send setQuestMembers (Json.Encode.encode 0 (Json.Encode.object [("name", string model.user.name), ("room", string model.room)])))
 
 -- SUBSCRIPTIONS
 
@@ -148,37 +265,93 @@ subscriptions model =
   [ WebSocket.listen (wsServer ++ model.room) NewMessage
   , WebSocket.listen roomGen GenRoom
   , WebSocket.listen joinRoom JoinRoom
+  , WebSocket.listen playerList PlayerList
+  , WebSocket.listen createRoles CreateRoles
+  , WebSocket.listen retrieveRole RetrieveRole
+  , WebSocket.listen charInfo CharInfo
   ]
   -- TODO: Add listeners for other
 
 -- VIEW
 
 playerOption maxPlayers =
-  option [ value (toString maxPlayers)] [ text (toString maxPlayers)]
+  option [ Html.Attributes.value (toString maxPlayers)] [ text (toString maxPlayers)]
 
 view : Model -> Html Msg
 view model =
-  if model.state == Home then
-    let
-      selectEvent =
-              on "change"
-                  (Json.map SetMaxPlayers targetValueIntParse)
-    in
-    div [][
-        input [ type_ "text", placeholder "Name", onInput Name, name "uname", value model.user.name] []
-        , input [ type_ "text", placeholder "Room Code", onInput RoomCode, name "roomcode", value model.room] []
-        , select [ selectEvent] (List.map playerOption (List.range 5 10))
-        , button [ type_ "button", onClick GenRoomCode] [text "Start a room"]
-        , button [ type_ "button", onClick SetRoom] [text "Join Game"]
+  case model.state of
+    Home ->
+      let
+        selectEvent =
+                on "change"
+                    (Json.Decode.map SetMaxPlayers targetValueIntParse)
+      in
+      div [][
+          input [ type_ "text", placeholder "Name", onInput Name, name "uname", Html.Attributes.value model.user.name] []
+          , input [ type_ "text", placeholder "Room Code", onInput RoomCode, name "roomcode", Html.Attributes.value model.room] []
+          , Html.select [ selectEvent] (List.map playerOption (List.range 5 10))
+          , button [ type_ "button", onClick GenRoomCode] [text "Start a room"]
+          , button [ type_ "button", onClick SetRoom] [text "Join Game"]
+          , div [] [text model.errors]
+          ]
+    Setup ->
+      div []
+        [ input [onInput Input, placeholder "Chat with others!"] []
+        , button [onClick Send] [text "Send"]
+        , div [] (List.map viewMessage (List.reverse model.chatMessages))
+        , div [] [text ("Your room code is " ++ model.room)]
+        , div [] [text ("The game will begin when " ++ (toString (model.maxPlayers - List.length model.currentPlayers)) ++ " players join")]
+        , div [] [text (model.revealedInfo)]
+        , div [] [text model.errors]
         ]
-  else
-    div []
+    TeamBuild ->
+      let
+        selectionBlock =
+          if getListPosition model.currentPlayers model.leaderPosition ==  model.user.name then
+            [div [] [ text ("You're the current quest leader. Please select " ++ toString model.quest.playersRequired ++ " players to go on the quest")]
+            ,div [] [(fieldset [] (List.map createCheckboxes model.currentPlayers))]
+            ]
+          else
+            [div [] [text ("Waiting for "++ getListPosition model.currentPlayers model.leaderPosition ++ " to finish selecting a team")]]
+      in
+      div []
       [ input [onInput Input, placeholder "Chat with others!"] []
       , button [onClick Send] [text "Send"]
       , div [] (List.map viewMessage (List.reverse model.chatMessages))
-      , div [] [text ("Your room code is " ++ model.room)]
+      , div [] [text (model.revealedInfo)]
+      , div [] selectionBlock
+      , div [] [text model.errors]
       ]
+    Vote -> div []
+      [ input [onInput Input, placeholder "Chat with others!"] []
+      , button [onClick Send] [text "Send"]
+      , div [] (List.map viewMessage (List.reverse model.chatMessages))
+      , div [] [text (model.revealedInfo)]
+      , div [] [text model.errors]
+      ]
+    Decide -> div []
+      [ input [onInput Input, placeholder "Chat with others!"] []
+      , button [onClick Send] [text "Send"]
+      , div [] (List.map viewMessage (List.reverse model.chatMessages))
+      , div [] [text (model.revealedInfo)]
+      , div [] [text model.errors]
+      ]
+    Wait -> div []
+      [ input [onInput Input, placeholder "Chat with others!"] []
+      , button [onClick Send] [text "Send"]
+      , div [] (List.map viewMessage (List.reverse model.chatMessages))
+      , div [] [text (model.revealedInfo)]
+      , div [] [text model.errors]
+      ]
+    Final -> div []
+      [ input [onInput Input, placeholder "Chat with others!"] []
+      , button [onClick Send] [text "Send"]
+      , div [] (List.map viewMessage (List.reverse model.chatMessages))]
 
 viewMessage : String -> Html msg
 viewMessage msg =
   div [] [ text msg ]
+
+createCheckboxes : String -> Html Msg
+createCheckboxes msg =
+  label [] [ text msg, input [ type_ "checkbox", onClick (UpdateQuestTeam msg)] [] ]
