9e73d20ee2331f254d8ab91827331aa42cfff578
Steve Herrmann <steven.herrmannwm@gmail.com>
2017-04-28 23:13:54 -0600

    Copied heroku server.js for testing

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
-var express = require('express');
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+/*var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
 
@@ -244,3 +267,4 @@ app.ws('/char_info', function(ws, req) {
 });
 
 app.listen(8080);
+*/
