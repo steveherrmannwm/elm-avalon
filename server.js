164604de3cee67d766c499b25a14f7491f625487
Steve Herrmann <steven.herrmannwm@gmail.com>
2017-04-29 00:54:47 -0600

    Fixed the server?

--- server.js
+++ server.js
@@ -105,14 +105,50 @@ wss.on('connection', (ws) => {
 
   switch (location){
     case "/gen_room":
-      ws.on('message', function(msg) {
-        var code = "";
-        while(code == "" || Object.keys(rooms).indexOf(code) > 1){
-          code = randomString(5, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        }
-        ws.send(code)
-        rooms[code] = {"users": {}, "roles": false}
-      });
+    ws.on('message', function(msg) {
+      var code = "";
+      while(code == "" || Object.keys(rooms).indexOf(code) > 1){
+        code = randomString(5, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+      }
+      ws.send(code)
+      rooms[code] = {"users": {}, "roles": false}
+
+      app.ws('/' + code, function(ws, req) {
+        ws.on('message', function(msg){
+            var parsed = JSON.parse(msg)
+            if(findObject(ws, rooms[code]) < 0)
+            {
+              rooms[code]["users"][parsed["name"]] = {"connections": {"chat": ws}};
+            }
+            for(var key in rooms[code]["users"]){
+              rooms[code]["users"][key]["connections"]["chat"].send(parsed["name"] + ": " + parsed["msg"])
+            }
+
+
+          })
+        ws.on('close', function(){
+          // Remove the connection from the list, and broadcast a message to other clients
+          var dc = ""
+          for(var key in rooms[code]["users"]){
+            if (rooms[code]["users"][key]["connections"]["chat"] === ws)
+            {
+              delete rooms[code]["users"][key];
+              dc = key;
+            }
+          }
+
+          for(var key in rooms[code]["users"]){
+            rooms[code]["users"][key]["connections"]["chat"].send(dc + " has disconnected")
+          }
+
+          if(Object.keys(rooms[code]["users"]).length == 0)
+          {
+            delete rooms[code]
+            console.log(code + " ended");
+          }
+        })
+        })
+    });
       break;
     case "/join_room":
       ws.on('message', function(msg) {
