d4afb94e9cf36fbda88b8ec4eaa1d7c630129d44
Steve Herrmann <steven.herrmannwm@gmail.com>
2017-05-03 00:22:52 -0600

    Set arbitrarily large timeout for testing purposes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const INDEX = path.join(__dirname, 'index.html');
 const server = express()
   .use((req, res) => {
     res.sendFile(INDEX);
-    req.setTimeout(0);
+    req.setTimeout(60 * 1000 * 20);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
