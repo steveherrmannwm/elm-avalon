bd89979ae3607847c8fe818bd591b59aaeb4f78b
Steve Herrmann <steven.herrmannwm@gmail.com>
2017-05-02 19:21:27 -0600

    Server changes

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+  .use((req, res) => {
+    res.sendFile(INDEX);
+    req.setTimeout(0);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
